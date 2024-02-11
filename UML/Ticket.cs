ed2fa225060c13d0f7fa50fe04c5a569f00fba60
bayushidzen <zhek_wh@mail.ru>
2024-02-11 16:23:53 +0300

    Starting UML

---
+++ UML/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace UML
+{
+    internal class Ticket
+    {
+        int rootNumber;
+        int price;
+        DateTime dataStart;
+        DateTime dataEnd;
+        int place;
+        bool isValid;
+
+        public void NewTicket(DateTime dataStart, DateTime dataEnd, int price, int place, bool isValid)
+        {
+            rootNumber++;
+            this.dataStart = dataStart;
+            this.dataEnd = dataEnd;
+            this.price = price;
+            this.place = place;
+            this.isValid = true;
+        }
+    }
+}
